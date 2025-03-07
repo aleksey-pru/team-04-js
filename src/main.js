15a05a381a2dfea04b124a72ebf4236a44109b11
Oleksii <o.pruzhanskiy@gmail.com>
2025-03-07 11:16:59 +0200

    update js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/about-me';
+import './js/covers';
+import './js/faq';
+import './js/header';
+import './js/projects';
+import './js/reviews';
+import './js/work-together';
+import './js/API';
