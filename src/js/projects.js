c574cda81b9dbd6e080cc14120ea4dd7a976ea0d
AnnaTarnavska <anja.t0410@gmail.com>
2025-03-10 20:35:42 +0200

    upd projects

---
+++ src/js/projects.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.projects-swiper', {
+  loop: false,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+      enabled: true,
+       releaseOnEdges: true,
+  },
+  touchEventsTarget: 'container',
+  navigation: {
+    nextEl: '.swiper-btn-right',
+    prevEl: '.swiper-btn-left',
+    disabledClass: 'swiper-btn-disabled',
+  },
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 1,
+//     },
+//     1440: {
+//       slidesPerView: 1,
+//     }
+//     },
+  watchOverflow: true,
+});
+
+swiper.on('slideChange', function () {
+  const prevButton = document.querySelector('.swiper-btn-left');
+  const nextButton = document.querySelector('.swiper-btn-right');
+
+  if (swiper.isBeginning) {
+    prevButton.classList.add('swiper-btn-disabled');
+  } else {
+    prevButton.classList.remove('swiper-btn-disabled');
+  }
+
+  if (swiper.isEnd) {
+    nextButton.classList.add('swiper-btn-disabled');
+  } else {
+    nextButton.classList.remove('swiper-btn-disabled');
+  }
+});
